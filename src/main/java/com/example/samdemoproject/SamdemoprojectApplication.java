5b218ad44418cd3374d76c2f4848b435742d5c64
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-26 21:33:58 -0500

    first commit

---
+++ src/main/java/com/example/samdemoproject/SamdemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.samdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamdemoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamdemoprojectApplication.class, args);
+    }
+
+}
