1911b7ca2eb9adc83743f603975fa1dddf8889a7
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-27 23:59:36 -0500

    finish create methods

--- src/main/java/com/example/samdemoproject/controllers/ProductController.java
+++ src/main/java/com/example/samdemoproject/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.example.samdemoproject.models.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +25,23 @@ public class ProductController {
         List<Product> products = productDao.findAll();
         model.addAttribute("products", products);
         return "product/viewAll";
+    }
+
+    @GetMapping("/product/create")
+    public String showCreateForm() {
+        return "product/create";
+    }
+
+    @PostMapping("/product/create")
+    public String saveCreateForm(
+         @RequestParam(name = "name") String name,
+         @RequestParam(name = "description") String description,
+         @RequestParam(name = "price") double price,
+         @RequestParam(name = "image") String image
+    ) {
+        Product productToAdd = new Product(name, description, price, image);
+        Product productInDB = productDao.save(productToAdd);
+
+        return "redirect:/product/" + productInDB.getId();
     }
 }
